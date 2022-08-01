a56b4cca2c8c950b7fcfed2e1b4162a21484cbcc
krupal <bhuvakrupal01@gmail.com>
2022-08-01 16:19:19 +0530

    ADD : How to use gRPC connectivity ( Not Working without server )

--- app/src/main/java/com/example/grpc_demo/MainActivity.kt
+++ app/src/main/java/com/example/grpc_demo/MainActivity.kt
@@ -11,10 +11,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.grpc_demo.ui.theme.GRPC_DemoTheme
+import io.grpc.ManagedChannelBuilder
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // this is just a demo how we can implement gRPC in android , this is not run on device because of we have not a sever
+        val managedChannel = ManagedChannelBuilder.forAddress("https://www.google.com", 443).build()
+        // ^ here we get Invalid host or port bcoz we have to use server ip or host
+        val blockingStub = BookServiceGrpc.newBlockingStub(managedChannel)
+        // if you are not found BookServiceGrpc or BookProto class than just build project once and it will create
+        val bookRequest = BookProto.GetBookRequest.newBuilder().setIsbn(9123471293487).build()
+        val book = blockingStub.getBook(bookRequest)
+
         setContent {
             GRPC_DemoTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Greeting(book.author)
                 }
             }
         }
